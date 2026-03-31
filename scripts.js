aba86f3370a84132dfbb0ce7262ca156e5031089
pet.araujo <pethrusaraujo8@gmail.com>
2026-03-30 22:49:34 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function inserttodisplay(data) {
+    document.querySelector('#display').value += data
+
+}
+
+function clean() {
+    document.querySelector('#display').value = ''
+}
+
+function back() {
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+}
+
+function result(){
+     const display = document.querySelector('#display')
+ try{
+display.value = eval(display.value)
+ } catch {
+    display.value = 'error'
+
+ }
+
+
+}
